08e16e9ec4db2298708e047b191934934bc68206
avinashvijayvargiya <avinash14022002@gmail.com>
2024-09-29 16:34:58 +0530

    initial commit

---
+++ src/main/java/com/github/avivijay19/apiservicebundle/ApiServiceBundleApplication.java
@@ -0,0 +1,13 @@
+package com.github.avivijay19.apiservicebundle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceBundleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiceBundleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/avivijay19/apiservicebundle/ApiServiceBundleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.avivijay19.apiservicebundle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceBundleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
